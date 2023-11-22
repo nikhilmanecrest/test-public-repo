ada1192ad7b9c81b732c86d9688ff0074e55cc67
Nikhil Mane <nikhil.mane@crestdatasys.com>
2023-11-22 14:05:08 +0000

    ieft7q

--- custom_viz/Pie_chart.js
+++ custom_viz/Pie_chart.js
@@ -30,6 +30,7 @@
             // Generate the chart data
             var FinalData = [];
             var finalLabel = [];
+            var other_links = [];
             let sum = 0;
             values.forEach(function (currentValue, index) {
                 var cell = currentValue[queryResponse.fields.measure_like[0].name];
@@ -39,12 +40,13 @@
                     FinalData.push(cell);
                     finalLabel.push(currentValue[queryResponse.fields.dimensions[0].name].value);
                 } else {
+                    other_links.push(cell.links[0])
                     sum = sum + currentValue[queryResponse.fields.measure_like[0].name].value;
                 }
             })
             // console.log(sum)
             finalLabel.push("Other")
-            FinalData.push(sum)
+            FinalData.push({rendered: sum.toString(), links: other_links, value:sum})
             var finalCharData = {
                 datasets: [{ data: FinalData }],
                  labels: finalLabel
