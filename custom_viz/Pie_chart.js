777c900c959e59446584ed8abf383420d8fffe17
Nikhil Mane <nikhil.mane@crestdatasys.com>
2023-11-22 09:33:08 +0000

    removed comments

--- custom_viz/Pie_chart.js
+++ custom_viz/Pie_chart.js
@@ -25,25 +25,11 @@ looker.plugins.visualizations.add({
                     },
                     onClick: function (event, elements) {
                         if (elements && elements.length > 0) {
-                            # // Trigger drill-down
                             console.log(elements, event);
                             var data = event.chart.config._config.data;
                             var label = data.labels;
                             var value = data.datasets[0].data;
                             console.log(data, label, value)
-                            # // Replace 'additional__fields' and 'event_counts' with your actual dimension and measure names
-                            # // looker.plugins.drillmenu.drill({
-                            # //   links: [{ label: label, type: 'dimension', type_label: 'Dimension', fields: ['additional__fields'] }],
-                            # //   data: [{ name: 'event_counts', value: value }],
-                            # // });
-                            # // function(event) {
-                            # // var activePoints = this.chart.getElementsAtEvent(event)
-                            # // console.log("kk")
-                            # // console.log(event.x)
-                            # LookerCharts.Utils.openDrillMenu({
-                            #     links: value[elements[0].index].links,
-                            #     event: { pageX: event.x, pageY: event.y }
-                            # });
                             console.log(value[elements[0].index].links)
                         }
                     }
