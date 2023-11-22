3e559012c2f6c7bec15ed64ea980da3733a93874
Nikhil Mane <nikhil.mane@crestdatasys.com>
2023-11-22 08:56:48 +0000

    created canvas

--- custom_viz/Pie_chart.js
+++ custom_viz/Pie_chart.js
@@ -1,5 +1,21 @@
 looker.plugins.visualizations.add({
+  id: "Pie_chart",
+  label: "Pie_chart",
+  options: {
+    font_size: {
+      type: "string",
+      label: "Font Size",
+      values: [
+        {"Large": "large"},
+        {"Small": "small"}
+      ],
+      display: "radio",
+      default: "large"
+    }
+  },
     create: function (element, config) {
+      var canvas = document.createElement('canvas');
+      element.appendChild(canvas);
     },
 
     updateAsync: function (data, element, config, queryResponse, details, done) {
