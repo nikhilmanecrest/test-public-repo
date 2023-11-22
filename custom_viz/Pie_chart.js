80f80cce5088beeb16cd83b5222d70f47886be26
Nikhil Mane <nikhil.mane@crestdatasys.com>
2023-11-22 10:15:42 +0000

    wfgiwef

--- custom_viz/Pie_chart.js
+++ custom_viz/Pie_chart.js
@@ -14,8 +14,6 @@
             return true;
         },
         create: function (element, config) {
-            console.log("in create");
-            // console.log(element, config);
             // Create a canvas element for the chart
             var canvas = document.createElement('canvas');
             canvas.setAttribute('id', 'customPieChartCanvas');
@@ -27,61 +25,30 @@
             });
         },
         update: function (data, element, config, queryResponse) {
-            // console.log(data, element, config, queryResponse);
             // Extract the data from Looker response
             var values = data;
-            // console.log(values);
             // Generate the chart data
-            var chartData = {
-                datasets: [{
-                    data: values.map(function (d) {
-                        return d[queryResponse.fields.measure_like[0].name].value;
-                    }),
-                    // backgroundColor: Object.values(Utils.CHART_COLORS)
-                }],
-                labels: values.map(function (d, index) {
-                    if (index < 19) {
-                        return d[queryResponse.fields.dimensions[0].name].value;
-                    } else {
-                        return "Other";
-                    }
-
-                })
-            };
             var FinalData = [];
             var finalLabel = [];
             let sum = 0;
-            var other_links = [];
             values.forEach(function (currentValue, index) {
                 var cell = currentValue[queryResponse.fields.measure_like[0].name];
                 if (index < 19) {
-
                     var cellElement = '<p>' + cell.value + ' </p>';
-                    cellElement.onclick = function (event) {
-                        LookerCharts.Utils.openDrillMenu({
-                            links: cell.links,
-                            event: event
-                        });
-                    };
                     htmlData = LookerCharts.Utils.htmlForCell(cell)
-                    // console.log(htmlData)
                     FinalData.push(cell);
                     finalLabel.push(currentValue[queryResponse.fields.dimensions[0].name].value);
                 } else {
-                    other_links.push(cell.links[0])
-
                     sum = sum + currentValue[queryResponse.fields.measure_like[0].name].value;
                 }
             })
             console.log(sum)
             finalLabel.push("Other")
-            FinalData.push({ rendered: sum.toString(), links: other_links, value: sum })
             var finalCharData = {
                 datasets: [{ data: FinalData }],
-                // labels: finalLabel
+                 labels: finalLabel
             }
             console.log(finalCharData)
-            //console.log(chartData)
             // Update the chart with the data
             this.chart.data = finalCharData;
 
