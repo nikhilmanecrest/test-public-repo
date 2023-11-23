101bd103f0bb372d6f7370647f8123c57c4a3e8c
Nikhil Mane <nikhil.mane@crestdatasys.com>
2023-11-23 12:36:59 +0000

    wefewf

--- custom_viz/custom_table_viz.js
+++ custom_viz/custom_table_viz.js
@@ -1,25 +1,79 @@
 looker.plugins.visualizations.add({
-  create: function(element, config) {
-    // Create a container element for the visualization
-    element.innerHTML = '<div id="custom-table"></div>';
-
-    // Render the table using LookerCharts
-    this.chart = LookerCharts.Utils.createVis(element, config, {
-      type: 'table'
-    });
+  // Id and Label are legacy properties that no longer have any function besides documenting
+  // what the visualization used to have. The properties are now set via the manifest
+  // form within the admin/visualizations page of Looker
+  id: "hello_world",
+  label: "Hello World",
+  options: {
+    font_size: {
+      type: "string",
+      label: "Font Size",
+      values: [
+        {"Large": "large"},
+        {"Small": "small"}
+      ],
+      display: "radio",
+      default: "large"
+    }
   },
+  // Set up the initial state of the visualization
+  create: function(element, config) {
 
-  update: function(data, element, config, queryResponse) {
-    // Access the data for the first cell in the first row
-    var cellData = data[0][0].value;
+    // Insert a <style> tag with some styles we'll use later.
+    element.innerHTML = `
+      <style>
+        .hello-world-vis {
+          /* Vertical centering */
+          height: 100%;
+          display: flex;
+          flex-direction: column;
+          justify-content: center;
+          text-align: center;
+        }
+        .hello-world-text-large {
+          font-size: 72px;
+        }
+        .hello-world-text-small {
+          font-size: 18px;
+        }
+      </style>
+    `;
 
-    // Use LookerCharts.Utils.htmlForCell() to customize the cell content
-    var customHTML = LookerCharts.Utils.htmlForCell(
-      '<b>Custom Content:</b> ' + cellData,
-      cellData
-    );
+    // Create a container element to let us center the text.
+    var container = element.appendChild(document.createElement("div"));
+    container.className = "hello-world-vis";
 
-    // Update the content of the cell in the table
-    document.getElementById('custom-table').innerHTML = customHTML;
+    // Create an element to contain the text.
+    this._textElement = container.appendChild(document.createElement("div"));
+
+  },
+  // Render in response to the data or settings changing
+  updateAsync: function(data, element, config, queryResponse, details, done) {
+
+    // Clear any errors from previous updates
+    this.clearErrors();
+
+    // Throw some errors and exit if the shape of the data isn't what this chart needs
+    if (queryResponse.fields.dimensions.length == 0) {
+      this.addError({title: "No Dimensions", message: "This chart requires dimensions."});
+      return;
+    }
+
+    // Grab the first cell of the data
+    var firstRow = data[0];
+    var firstCell = firstRow[queryResponse.fields.dimensions[0].name];
+
+    // Insert the data into the page
+    this._textElement.innerHTML = LookerCharts.Utils.htmlForCell(firstCell);
+
+    // Set the size to the user-selected size
+    if (config.font_size == "small") {
+      this._textElement.className = "hello-world-text-small";
+    } else {
+      this._textElement.className = "hello-world-text-large";
+    }
+
+    // We are done rendering! Let Looker know.
+    done()
   }
 });
