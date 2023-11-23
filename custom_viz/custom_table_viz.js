806c974c1152f15ef06bff8319074e7d5e4a7a4c
Nikhil Mane <nikhil.mane@crestdatasys.com>
2023-11-23 12:43:23 +0000

    sgerg

--- custom_viz/custom_table_viz.js
+++ custom_viz/custom_table_viz.js
@@ -1,79 +1,39 @@
 looker.plugins.visualizations.add({
-  // Id and Label are legacy properties that no longer have any function besides documenting
-  // what the visualization used to have. The properties are now set via the manifest
-  // form within the admin/visualizations page of Looker
-  id: "hello_world",
-  label: "Hello World",
-  options: {
-    font_size: {
-      type: "string",
-      label: "Font Size",
-      values: [
-        {"Large": "large"},
-        {"Small": "small"}
-      ],
-      display: "radio",
-      default: "large"
-    }
-  },
-  // Set up the initial state of the visualization
-  create: function(element, config) {
-
-    // Insert a <style> tag with some styles we'll use later.
-    element.innerHTML = `
-      <style>
-        .hello-world-vis {
-          /* Vertical centering */
-          height: 100%;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          text-align: center;
+    create: function (element, config) {
+        console.log("Create function started..")
+        var chart = document.createElement('table');
+        chart.id = 'custom-table-chart';
+        element.appendChild(chart);
+        var style = document.createElement('style');
+        style.innerHTML = `table, th, td {
+          border: 1px solid black;
+          border-collapse: collapse;
+        }`
+        element.appendChild(style);
+        console.log("chart element added...")
+    },
+    updateAsync: function (data, element, config, queryResponse, details, doneRendering) {
+        console.log("update function started..")
+        var chart = element.querySelector('#custom-table-chart');
+        chart.innerHTML = '';
+        var headerRow = document.createElement('tr');
+        for (var i of queryResponse.fields.dimensions) {
+            var th = document.createElement('th');
+            th.textContent = i.name;
+            headerRow.appendChild(th);
         }
-        .hello-world-text-large {
-          font-size: 72px;
-        }
-        .hello-world-text-small {
-          font-size: 18px;
-        }
-      </style>
-    `;
-
-    // Create a container element to let us center the text.
-    var container = element.appendChild(document.createElement("div"));
-    container.className = "hello-world-vis";
-
-    // Create an element to contain the text.
-    this._textElement = container.appendChild(document.createElement("div"));
-
-  },
-  // Render in response to the data or settings changing
-  updateAsync: function(data, element, config, queryResponse, details, done) {
-
-    // Clear any errors from previous updates
-    this.clearErrors();
-
-    // Throw some errors and exit if the shape of the data isn't what this chart needs
-    if (queryResponse.fields.dimensions.length == 0) {
-      this.addError({title: "No Dimensions", message: "This chart requires dimensions."});
-      return;
+        chart.appendChild(headerRow);
+        // Create table rows
+        data.forEach(function (row) {
+            var rows = document.createElement('tr');
+            Object.keys(row).forEach(function (key) {
+                var td = document.createElement('td');
+                td.textContent = row[key].value;
+                rows.appendChild(td);
+            });
+            chart.appendChild(rows);
+        });
+        element.appendChild(chart);
+        doneRendering()
     }
-
-    // Grab the first cell of the data
-    var firstRow = data[0];
-    var firstCell = firstRow[queryResponse.fields.dimensions[0].name];
-
-    // Insert the data into the page
-    this._textElement.innerHTML = LookerCharts.Utils.htmlForCell(firstCell);
-
-    // Set the size to the user-selected size
-    if (config.font_size == "small") {
-      this._textElement.className = "hello-world-text-small";
-    } else {
-      this._textElement.className = "hello-world-text-large";
-    }
-
-    // We are done rendering! Let Looker know.
-    done()
-  }
 });
