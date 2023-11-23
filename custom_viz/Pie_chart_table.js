3b8acff8d093729fe60eaf0c18f00978fd623f20
Nikhil Mane <nikhil.mane@crestdatasys.com>
2023-11-23 10:52:54 +0000

    dcnbdfebf

--- custom_viz/Pie_chart_table.js
+++ custom_viz/Pie_chart_table.js
@@ -11,21 +11,33 @@
         var headerCell2 = headerRow.insertCell(1);
         headerCell1.innerHTML = 'Product Name';
         headerCell2.innerHTML = 'Count';
-
         // Add borders to header cells
         headerCell1.style.border = '1px solid #ccc';
         headerCell2.style.border = '1px solid #ccc';
-
+        if(selectedData.length==1){
         // Create table row for selected data
-        var row = table.insertRow(1);
+        var row = table.insertRow(-1);
         var cell1 = row.insertCell(0);
         var cell2 = row.insertCell(1);
         cell1.innerHTML = selectedData[0][0];
         cell2.innerHTML = selectedData[0][1];
-
+        }
+        else {
+        var len=0;
+        console.log(selectedData[0][0])
+        selectedData.forEach(()=>{
+          // Create table row for selected data
+        var row = table.insertRow(-1);
+        var cell1 = row.insertCell(0);
+        var cell2 = row.insertCell(1);
+        cell1.innerHTML = selectedData[len][0];
+        cell2.innerHTML = selectedData[len][1];
         // Add borders to data cells
         cell1.style.border = '1px solid #ccc';
         cell2.style.border = '1px solid #ccc';
+        len=len+1
+        })
+        }
     }
 
     looker.plugins.visualizations.add({
@@ -78,9 +90,18 @@
                             data=[]
                             var label_first_column=label[elements[0].index];
                             var value_first_measure = value[elements[0].index].value;
-                            data.push([label_first_column,value_first_measure])
-                            // Display selected data in the table
-                            updateTable(data);
+                            if (elements[0].index ==19){
+                                    label[20].forEach((element)=>{
+                                          data.push([element[0],element[1]])
+                                    })
+                            }
+                            else
+                            {
+                                    data.push([label_first_column,value_first_measure])
+
+                            }
+                                // Display selected data in the table
+                                    updateTable(data);
                         }
                     }
                 }
@@ -93,6 +114,7 @@
             var FinalData = [];
             var finalLabel = [];
             var other_links = [];
+            var other_data = []
             let sum = 0;
             values.forEach(function (currentValue, index) {
                 var cell = currentValue[queryResponse.fields.measure_like[0].name];
@@ -102,10 +124,15 @@
                     finalLabel.push(currentValue[queryResponse.fields.dimensions[0].name].value);
                 } else {
                     other_links.push(cell.links[0])
+                    record=[]
+                    record.push(currentValue[queryResponse.fields.dimensions[0].name].value)
+                    record.push(currentValue[queryResponse.fields.measure_like[0].name].value);
+                    other_data.push(record)
                     sum = sum + currentValue[queryResponse.fields.measure_like[0].name].value;
                 }
             })
             finalLabel.push("Other")
+            finalLabel.push(other_data)
             FinalData.push({ rendered: sum.toString(), links: other_links, value: sum })
             var finalCharData = {
                 datasets: [{ data: FinalData }],
