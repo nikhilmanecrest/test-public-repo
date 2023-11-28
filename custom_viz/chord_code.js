cf224b5e52ddae9bda7f95940d96277b2608b3e0
Nikhil Mane <nikhil.mane@crestdatasys.com>
2023-11-28 11:32:25 +0000

    4r4r

--- custom_viz/chord_code.js
+++ custom_viz/chord_code.js
@@ -1,5 +1,3 @@
-// Custom Chord Visualization for Looker
-
 looker.plugins.visualizations.add({
   create: function (element, config) {
     // Create a container element for the visualization
@@ -9,9 +7,15 @@ looker.plugins.visualizations.add({
     // Initialize the visualization properties
     this.chart = d3.select(container).append("svg");
 
+    // Apply styles to the container
+    container.style.width = "100%";
+    container.style.height = "100%";
+    container.style.overflow = "scroll";
+
     // Apply styles to the SVG
     this.chart.style("width", "100%");
     this.chart.style("height", "100%");
+    this.chart.style("overflow-x", "scroll");
   },
 
   updateAsync: function (data, element, config, queryResponse, details, doneRendering) {
@@ -49,25 +53,66 @@ looker.plugins.visualizations.add({
     var ribbon = d3.ribbon()
       .radius(innerRadius);
 
-    // Create a color scale
+    // Create a color scale with a vibrant color scheme
     var colorScale = d3.scaleOrdinal(d3.schemeCategory10);
 
     // Create chord data
     var chordData = chord(matrix);
 
-    // Draw chords
-    this.chart.append("g")
+    // Center the chart within its container
+    var svg = this.chart.attr("width", "100%")
+      .attr("height", "100%")
+      .append("g")
+      .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
+
+    // Draw chords with gradient fill for a wow effect
+    svg.append("g")
       .selectAll("path")
       .data(chordData)
       .enter().append("path")
       .attr("d", ribbon)
       .style("fill", function (d) {
-        return colorScale(d.source.index);
+        return "url(#gradient" + d.source.index + ")";
       })
-      .style("stroke", "black");
+      .style("stroke", "white")
+      .style("stroke-width", "1px");
 
-    // Draw groups
-    var group = this.chart.append("g")
+    // Define gradients for each chord
+    var gradients = svg.append("defs").selectAll("linearGradient")
+      .data(chordData)
+      .enter().append("linearGradient")
+      .attr("id", function (d) {
+        return "gradient" + d.source.index;
+      })
+      .attr("gradientUnits", "userSpaceOnUse")
+      .attr("x1", function (d) {
+        return Math.cos((d.source.endAngle - d.source.startAngle) / 2 + d.source.startAngle - Math.PI / 2);
+      })
+      .attr("y1", function (d) {
+        return Math.sin((d.source.endAngle - d.source.startAngle) / 2 + d.source.startAngle - Math.PI / 2);
+      })
+      .attr("x2", function (d) {
+        return Math.cos((d.target.endAngle - d.target.startAngle) / 2 + d.target.startAngle - Math.PI / 2);
+      })
+      .attr("y2", function (d) {
+        return Math.sin((d.target.endAngle - d.target.startAngle) / 2 + d.target.startAngle - Math.PI / 2);
+      });
+
+    // Set color stops for gradients
+    gradients.append("stop")
+      .attr("offset", "0%")
+      .attr("style", function (d) {
+        return "stop-color:" + colorScale(d.source.index);
+      });
+
+    gradients.append("stop")
+      .attr("offset", "100%")
+      .attr("style", function (d) {
+        return "stop-color:" + colorScale(d.target.index);
+      });
+
+    // Draw groups with vibrant fill
+    var group = svg.append("g")
       .selectAll("g")
       .data(chordData.groups)
       .enter().append("g");
@@ -76,7 +121,8 @@ looker.plugins.visualizations.add({
       .style("fill", function (d) {
         return colorScale(d.index);
       })
-      .style("stroke", "black")
+      .style("stroke", "white")
+      .style("stroke-width", "1px")
       .attr("d", arc);
 
     group.append("text")
@@ -94,7 +140,8 @@ looker.plugins.visualizations.add({
       })
       .text(function (d) {
         return dimensionNames[d.index];
-      });
+      })
+      .style("fill", "white");
 
     // Signal that the rendering is complete
     doneRendering();
