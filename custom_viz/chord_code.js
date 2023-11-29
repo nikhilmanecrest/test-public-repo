449d270c4c2f371874911360252a4ae664a5d158
Nikhil Mane <nikhil.mane@crestdatasys.com>
2023-11-29 06:27:30 +0000

    sfef

--- custom_viz/chord_code.js
+++ custom_viz/chord_code.js
@@ -1,9 +1,4 @@
-// Function to create the Liquid Fill Gauge
 function createLiquidFillGauge(container, value, config) {
-  // D3.js implementation for a simple liquid fill gauge
-  // Customize based on your needs
-
-  // Example D3.js code
   const svg = d3.select(container)
     .append("svg")
     .attr("width", config.width)
@@ -19,29 +14,56 @@ function createLiquidFillGauge(container, value, config) {
     .attr("r", config.radius)
     .style("fill", config.fillColor);
 
-  // Add code to update the fill based on the value from Looker data
-  const fillValue = /* Get value from Looker data */;
-  fill.transition()
+  const text = gauge.append("text")
+    .attr("text-anchor", "middle")
+    .attr("dy", ".35em")
+    .style("font-size", "20px");
+
+  const arc = d3.arc()
+    .startAngle(0)
+    .endAngle(Math.PI);
+
+  const foreground = gauge.append("path")
+    .datum({ endAngle: 0 })
+    .attr("class", "foreground")
+    .attr("d", arc)
+    .style("fill", config.fillColor);
+
+  foreground.transition()
     .duration(config.transitionDuration)
-    .attrTween("transform", (d, i, a) => d3.interpolateString(a, `rotate(${fillValue * 180} 0 0)`));
+    .attrTween("d", function (d) {
+      const interpolate = d3.interpolate(d.endAngle, value * Math.PI);
+      return function (t) {
+        d.endAngle = interpolate(t);
+        return arc(d);
+      };
+    });
+
+  text.text(`${Math.round(value * 100)}%`);
 }
 
-// Looker Visualization Extension Initialization
 looker.plugins.visualizations.add({
   create: function (element, config) {
     element.innerHTML = '<div id="myLiquidFillGaugeContainer"></div>';
   },
   updateAsync: function (data, element, config, queryResponse, details, doneRendering) {
     const container = document.getElementById('myLiquidFillGaugeContainer');
-    const value = data[0][queryResponse.fields.dimensions[0].name].value;
-    const liquidFillConfig = {
-      width: 200,
-      height: 200,
-      radius: 80,
-      fillColor: "#3498db",
-      transitionDuration: 1000,
-    };
-    createLiquidFillGauge(container, value, liquidFillConfig);
+
+    if (data && data.length > 0) {
+      const value = data[0][queryResponse.fields.measure_like[0].name].value;
+      const liquidFillConfig = {
+        width: 200,
+        height: 200,
+        radius: 80,
+        fillColor: "#3498db",
+        transitionDuration: 1000,
+      };
+
+      createLiquidFillGauge(container, value, liquidFillConfig);
+    } else {
+      container.innerHTML = 'No data available.';
+    }
+
     doneRendering();
-  }
+  },
 });
