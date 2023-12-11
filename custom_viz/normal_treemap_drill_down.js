549c55cae93207be85ffae31e1c7eb36a4780ee3
Nikhil Mane <nikhil.mane@crestdatasys.com>
2023-12-11 13:26:49 +0000

    werl32ohr

--- custom_viz/normal_treemap_drill_down.js
+++ custom_viz/normal_treemap_drill_down.js
@@ -19,9 +19,9 @@ looker.plugins.visualizations.add({
     // create tooltip
     var tooltip=container.appendChild(document.createElement("div"));
     tooltip.id = "my-visualization-tooltip";
-    tooltip.style.width = "50%"; // Adjust width as needed
-    tooltip.style.height = "100%";
-    tooltip.style.display = "display"
+    //tooltip.style.width = "50%"; // Adjust width as needed
+    //tooltip.style.height = "100%";
+    //tooltip.style.display = "display"
     // Initialize the treemap visualization properties
     this.chart = d3.select(treemap).append("svg").attr("width", "100%").attr("height", "100%");
 
